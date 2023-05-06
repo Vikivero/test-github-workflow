882df2722801d60587f8ea712f10044255e7f76d
vikivero <vikichan.ch@gmail.com>
2023-05-06 21:43:37 +0200

    Corrected pylint findings

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+"""
+This is a simple Flask web application.
+"""
+
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
- return '<h1>Hello WSB! Greetings from Flask!</h1>'
+    """
+    Returns a greeting message as an HTML string.
+    """
+    return '<h1>Hello WSB! Greetings from Flask!</h1>'
+
 
 if __name__ == "__main__":
- app.run(debug=True)
+    app.run(debug=True)
